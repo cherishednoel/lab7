2831dcde1c180c9891670bff1323ac4ec318363d
cherishednoel <panida.stp@icloud.com>
2022-12-13 15:24:09 +0700

    lab7_2.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
-
 using namespace std;
 
 int main(){
-	
 	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
-		i++;
+
+	cout << "Please input k: ";
+	cin >> k;
+
+	if(k > 0){
+		i = 0, result = 0;
+		while(i < 5000){
+			P[i] = 2*i+1;
+			if(P[i] % k == 0){
+				result -= P[i];
+			} else{
+				result += P[i];
+			}
+			i++;
+		}
+		cout << "Result = " << result << endl;
+	} else{
+		cout << "Invalid input!!!" << endl;
 	}
 	
 	return 0;
